e351845a750ea0e8a6773a9b695b49b591233521
roytamba <roymartogit@gmail.com>
2025-05-05 15:07:31 +0700

    contact

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () { return view('portfolio.page'); })->name('welcome');
 Route::get('/resume', function () { return view('portfolio.resume'); })->name('resume');
+Route::get('/projects', function () { return view('portfolio.projects'); })->name('projects');
+Route::get('/contact', function () { return view('portfolio.contact'); })->name('contact');
 Route::get('/download/my-cv', [PortfolioController::class, 'downloadCv'])->name('download.cv');
