e1a27c086826ba41738303a4fef347d7dca8902a
roytamba <roymartogit@gmail.com>
2025-05-05 12:48:11 +0700

    about

--- resources/views/portfolio/components/about.blade.php
+++ resources/views/portfolio/components/about.blade.php
@@ -4,10 +4,15 @@
             <div class="col-xxl-8">
                 <div class="text-center my-5">
                     <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
-                    <p class="lead fw-light mb-4">My name is Start Bootstrap and I help brands grow.</p>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit
-                        dolorum itaque qui unde quisquam consequatur autem. Eveniet quasi nobis aliquid cumque
-                        officiis sed rem iure ipsa! Praesentium ratione atque dolorem?</p>
+                    <p class="lead fw-light mb-4">My name is Roy Martogi Tamba and I help brands grow.</p>
+                    <p class="text-muted">Hi i'm a Software Developer and Technical Support MDS (Managed
+                        Device Service) professional at Bizmann PTE LTD in Singapore. Additionally, I contribute to the
+                        operations of the company's subsidiary, PT Bisnis Teknologi Manajemen in Batam, Indonesia.
+
+                        In my role as a Software Developer, I focus on resolving issues and adding new features to our
+                        corporate website. I also actively participate in creating and implementing new projects. On the
+                        technical support side, I assist in integrating the software we develop into hardware
+                        components, such as printers.</p>
                     <div class="d-flex justify-content-center fs-2 gap-4">
                         <a class="text-gradient" href="#!"><i class="bi bi-twitter"></i></a>
                         <a class="text-gradient" href="#!"><i class="bi bi-linkedin"></i></a>
