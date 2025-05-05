f30442c4be0135ba3cc0dc24bc1021f6d2ade379
roytamba <roymartogit@gmail.com>
2025-05-05 14:35:19 +0700

    resume + cv

---
+++ app/Http/Controllers/PortfolioController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class PortfolioController extends Controller
+{
+    public function downloadCv()
+    {
+        $path = 'documents/MyCV.pdf';
+
+        if (!Storage::exists($path)) {
+            abort(404);
+        }
+
+        $fullPath = Storage::path($path);
+        return response()->file($fullPath, [
+            'Content-Type' => 'application/pdf',
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () { return view('portfolio.page'); })->name('welcome');
 Route::get('/resume', function () { return view('portfolio.resume'); })->name('resume');
+Route::get('/download/my-cv', [PortfolioController::class, 'downloadCv'])->name('download.cv');
