c606260054277466539c08732cd1b708682dc7d3
roytamba <roymartogit@gmail.com>
2025-05-05 14:11:18 +0700

    resume

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('portfolio.page');
-});
+Route::get('/', function () { return view('portfolio.page'); })->name('welcome');
+Route::get('/resume', function () { return view('portfolio.resume'); })->name('resume');
